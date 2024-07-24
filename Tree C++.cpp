5d5def309911e8ece6a8732df2a6cb436c784343
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-24 19:35:10 +0530

    Add files via upload

---
+++ Tree C++.cpp
@@ -0,0 +1,21 @@
+//Tree:- 
+
+#include<iostream>
+using namespace std;
+
+// This is how the node of a tree is defined //
+class Node
+{
+    public:
+
+    int data;
+    Node * left;
+    Node * right;
+
+    //Note : since each node has the address to the corresponding to th left and the right of the root, we are able to access the whole tree from the root node
+};
+int main()
+{
+    string CEO;
+    string PA, CFO;
+}
